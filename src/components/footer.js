caed93e0b6068f31ff3c34fcb501fdb89fb9c6f4
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-07-14 00:45:39 -0500

    completed homepage

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="navbar navbar-expand-lg navbar-dark bg-dark mt-5 p-0">
+        <div className="container d-flex justify-content-end">
+          <div className="navbar-nav">
+            <p className="nav-item nav-link mb-0">To0nscape, by <a className="text-white" href="https://github.com/samueltrejo">@samueltrejo</a>.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/home-games.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import GameCards from './game-cards';
+
+import games from '../helpers/games.json';
+
+class HomeGames extends React.Component {
+  render() {
+    const writeGameCards = games.map(game => (
+      <GameCards game={game} />
+    ));
+    return (
+      <div className="HomeGames container">
+        <div className="lead">Games</div>
+        <div className="row">
+          {writeGameCards}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomeGames;
