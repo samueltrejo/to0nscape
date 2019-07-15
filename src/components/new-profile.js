22bb16fb731c0c238b1f0d7924d1f52989336c52
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-07-14 22:50:58 -0500

    attached new profile form inputs to state and added onChange for updating

--- src/components/new-profile.js
+++ src/components/new-profile.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
+const newProfileDefault = {
+  username: '',
+  image: '',
+  bio: '',
+};
+
 class NewProfile extends React.Component {
+  state = {
+    newProfile: newProfileDefault,
+  }
+
+  stateProfileChange = (type, event) => {
+    const newProfileCopy = { ...this.state.newProfile };
+    newProfileCopy[type] = event.target.value;
+    console.error(newProfileCopy);
+    this.setState({ newProfile: newProfileCopy });
+  }
+
+  updateUsername = event => this.stateProfileChange('username', event)
+
+  updateImage = event => this.stateProfileChange('image', event)
+
+  updateBio = event => this.stateProfileChange('bio', event)
+
   render() {
+    const { username, image, bio } = this.state;
+    const { updateUsername, updateImage, updateBio } = this;
+
     return (
       <div className="NewProfile">
         <div className="col-12 vh-100 d-flex align-items-center px-0">
@@ -15,9 +41,9 @@ class NewProfile extends React.Component {
                   <div className="col-md-6">
                     <div className="card-body text-center mx-5">
                       <h5 className="lead font-weight-bold mb-5 text-center">Create Profile</h5>
-                      <input type="text" className="form-control mb-4" placeholder="Username" />
-                      <input type="text" className="form-control mb-4" placeholder="Profile Picture" />
-                      <textarea className="form-control mb-4" rows="4" placeholder="Bio"></textarea>
+                      <input type="text" className="form-control mb-4" placeholder="Username" value={username} onChange={updateUsername} />
+                      <input type="text" className="form-control mb-4" placeholder="Profile Picture" value={image} onChange={updateImage} />
+                      <textarea className="form-control mb-4" rows="4" placeholder="Bio" value={bio} onChange={updateBio}></textarea>
                       <button className="btn btn-dark px-3"><i className="fas fa-address-card pr-1"></i> Create</button>
                     </div>
                   </div>
