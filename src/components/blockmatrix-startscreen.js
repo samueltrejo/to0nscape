3c8aba80dd54c087bcd0e8dc5c8432f225c4046c
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-07-16 18:23:15 -0500

    added blockmatrix startscreen

--- src/components/blockmatrix-startscreen.js
+++ src/components/blockmatrix-startscreen.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class BlockMatrixStartscreen extends React.Component {
   render() {
     return (
-      <div className="BlockMatrixStartscreen">Block Matrix Startscreen</div>
+      <div className="BlockMatrixStartscreen">
+        {/* <div className="container pt-5"> */}
+          {/* <div className="bg-white"> */}
+            <div id="start-screen" className="col-4 mx-auto mt-5">
+              <div className="modal-content">
+                <h1 className="display-4 mx-auto mb-0 mt-3">Block Matrix</h1>
+                <h6 className="m-auto">by <a className="text-muted" href="https://github.com/samueltrejo" target="blank">@samueltrejo</a></h6>
+                <div className="modal-body">
+                  <div className="text-center">Use left and right arrows to dodge the obstacles. Have fun!</div>
+                </div>
+                <div className="modal-footer justify-content-center flex-column">
+                  <Link to="/blockmatrix" type="button" className="btn btn-dark rounded">Start Game</Link>
+                </div>
+              </div>
+            </div>
+          {/* </div> */}
+        {/* </div> */}
+      </div>
     );
   }
 }
--- src/components/blockmatrix.js
+++ src/components/blockmatrix.js
@@ -3,7 +3,11 @@ import React from 'react';
 class BlockMatrix extends React.Component {
   render() {
     return (
-      <div>Block Matrix</div>
+      <div className="BlockMatrix p-5 vh-100">
+        <div className="game-screen h-100 bg-dark position-relative">
+          <div className="player bg-white position-absolute"></div>
+        </div>
+      </div>
     );
   }
 }
