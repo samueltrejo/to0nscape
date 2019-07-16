f862c15170c2c372ea580dfd0c79482dcd89e971
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-07-15 19:56:27 -0500

    changing profile info to inputs on edit state change to false

--- src/components/my-profile.js
+++ src/components/my-profile.js
@@ -2,24 +2,33 @@ import React from 'react';
 import Navbar from './navbar';
 
 class MyProfile extends React.Component {
+  state = {
+    edit: false,
+    profile: this.props.profile,
+  }
+
   render() {
+    const { edit } = this.state;
     const { profile } = this.props;
     return (
       <div>
         <Navbar />
         <div className="hero-image overflow-hidden">
-          <img src="https://i.imgur.com/6QPY9Cz.jpg" className="img-fluid" alt="..."></img>
+          <img src="https://i.imgur.com/6QPY9Cz.jpg" className="img-fluid" alt="..." />
         </div>
         <div className="container">
           <div className="row">
 
             <div className="card col-2 p-0 m-0">
               <div className="profile-picture">
-                <img src={profile.image} className="card-img-top" alt="..." />
+                {edit ? (<input type="text" className="form-control mb-4" placeholder="Profile Picture" value={profile.image} onChange={this.updateImage} />)
+                  : (<img src={profile.image} className="card-img-top" alt="..." />)}
               </div>
               <div className="card-body">
-                <p className="lead">{profile.username}</p>
-                <p className="lead">{profile.bio}</p>
+                {edit ? (<input type="text" className="form-control mb-4" placeholder="Username" value={profile.username} onChange={this.updateUsername} />)
+                  : (<p className="lead">{profile.username}</p>)}
+                {edit ? (<input type="text" className="form-control mb-4" placeholder="Bio" value={profile.bio} onChange={this.updateUsername} />)
+                  : (<p className="lead">{profile.bio}</p>)}
               </div>
             </div>
 
