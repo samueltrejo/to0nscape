e6a26e8fc49e087d5a23b4209b3a50bbd18e80ee
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-07-20 13:30:34 -0500

    sorting and displaying scores

--- src/components/leaderboards.js
+++ src/components/leaderboards.js
@@ -3,12 +3,16 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import profileData from '../helpers/data/profile-data';
+import scoresData from '../helpers/data/scores-data';
 
 import Navbar from './navbar';
 
 class Leaderboards extends React.Component {
   state = {
     profile: {},
+    scores: [],
+    scoreCategories: [],
+    scoreCategory: 'Block Matrix',
   }
 
   getMyProfile = () => {
@@ -20,16 +24,75 @@ class Leaderboards extends React.Component {
     }
   }
 
+  getScores = () => {
+    scoresData.getScores()
+      .then((scores) => {
+        this.setState({ scores });
+        this.getScoreCategories();
+      })
+      .catch(error => console.error(error));
+  }
+
+  getScoreCategories = () => {
+    this.state.scores.forEach((score) => {
+      const { scoreCategories } = this.state;
+      if (!scoreCategories.includes(score.game)) {
+        scoreCategories.push(score.game);
+        this.setState({ scoreCategories });
+      }
+    });
+  }
+
+  setScoreCategory = (event) => {
+    const category = event.target.textContent;
+    this.setState({ scoreCategory: category });
+  }
+
   componentDidMount() {
     this.getMyProfile();
+    this.getScores();
   }
 
   render() {
-    const { profile } = this.state;
+    const {
+      profile,
+      scoreCategories,
+      scoreCategory,
+      scores,
+    } = this.state;
+    const writeScoreCategories = scoreCategories.map(category => (
+      <span class="dropdown-item" onClick={this.setScoreCategory}>{category}</span>
+    ));
+    const writeScores = scores.map((score) => {
+      let scoreElement = '';
+      if (score.game.includes(scoreCategory)) {
+        scoreElement = <li class="list-group-item">{score.username}, {score.score}</li>;
+      }
+      return scoreElement;
+    });
     return (
       <div className="Leaderboards">
         <Navbar authed={true} carousel={false} profile={profile} />
         <h3>Leaderboards</h3>
+
+        <div className="container mt-5">
+          <div class="card">
+            <div class="card-body">
+              <div class="dropdown">
+            <h5 class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="border-bottom border-dark">{scoreCategory}</span>
+            </h5>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              {writeScoreCategories}
+            </div>
+          </div>
+            </div>
+            <ul class="list-group list-group-flush">
+              {writeScores}
+            </ul>
+            <div class="card-body"></div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/helpers/data/scores-data.js
+++ src/helpers/data/scores-data.js
@@ -18,20 +18,20 @@ const databaseUrl = fbconfig.databaseURL;
 //     .catch(error => reject(error));
 // });
 
-// const getScores = () => new Promise((resolve, reject) => {
-//   axios.get(`${databaseUrl}/scores.json?orderBy="uid"&equalTo="${uid}"`)
-//     .then((response) => {
-//       let profile = {};
-//       if (response.data !== null) {
-//         Object.keys(response.data).forEach((profileKey) => {
-//           response.data[profileKey].id = profileKey;
-//           profile = response.data[profileKey];
-//         });
-//         resolve(profile);
-//       }
-//     })
-//     .catch(error => reject(error));
-// });
+const getScores = () => new Promise((resolve, reject) => {
+  axios.get(`${databaseUrl}/scores.json?`)
+    .then((response) => {
+      const scores = [];
+      if (response.data !== null) {
+        Object.keys(response.data).forEach((scoreKey) => {
+          response.data[scoreKey].id = scoreKey;
+          scores.push(response.data[scoreKey]);
+        });
+        resolve(scores);
+      }
+    })
+    .catch(error => reject(error));
+});
 
 const postScore = score => axios.post(`${databaseUrl}/scores.json`, score);
 
@@ -39,4 +39,4 @@ const postScore = score => axios.post(`${databaseUrl}/scores.json`, score);
 
 // const deleteProfile = profileId => axios.delete(`${databaseUrl}/profiles/${profileId}.json`);
 
-export default { postScore };
+export default { getScores, postScore };
